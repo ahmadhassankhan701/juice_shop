99882f99410106f88499505bfd5e98b1edb3fa9e
Ahmad Hassan Khan <56753313+Ahmadlohani@users.noreply.github.com>
2023-11-25 13:05:45 +0500

    Update firebase.js
    Made more secure

--- firebase.js
+++ firebase.js
@@ -5,13 +5,13 @@ import { getStorage } from "firebase/storage";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-	apiKey: "AIzaSyAB7ckwEQUhS1Xezm5PrPqpximHnvdnIVc",
-	authDomain: "juice-2854d.firebaseapp.com",
-	projectId: "juice-2854d",
-	storageBucket: "juice-2854d.appspot.com",
-	messagingSenderId: "274236396388",
-	appId: "1:274236396388:web:593de08cacb47af7136071",
-	databaseURL: "https://juice-2854d-default-rtdb.firebaseio.com/",
+	apiKey: "",
+	authDomain: "",
+	projectId: "",
+	storageBucket: "",
+	messagingSenderId: "",
+	appId: "",
+	databaseURL: "",
 };
 
 export const app = initializeApp(firebaseConfig, "juice");
